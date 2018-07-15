a6a3209ea5cae449376499fa57334972cb36bc5f
saitejabethini <teja_btn@hotmail.com>
2018-07-14 23:19:09 -0400

    Initial Commit of Ass2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,40 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     d <- det(x)
+     if(d == 0)
+     {
+          break()
+     } 
+     inverseTF <- NULL
+     setInverse <- function(y){
+          x <<- solve(y)
+          inverseTF <<- TRUE
+     }
+     
+     getInverse <- function() {
+          if(isTRUE(inverseTF))
+          {x}
+          else{
+               print('Inverse is not set yet. Please set inverse using makeCacheMatrix function')
+               break()
+          }
+          
+     }
+     setInverse(x)
+     ret <- matrix(c(setInverse = setInverse, getInverse = getInverse), nrow = 1, ncol = 2)
+     ret
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     ## Return a matrix that is the inverse of 'x'
+     inverse <- getInverse()
+     if(isTRUE(inverseTF)){
+          print('getting cached matrix')
+          return(inverse, solve(inverse))
+     }
+     
+     
 }
