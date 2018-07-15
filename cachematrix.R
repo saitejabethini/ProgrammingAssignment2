b415b8358cc64bdda257902bc6ec0add9570a307
saitejabethini <teja_btn@hotmail.com>
2018-07-15 03:43:22 -0400

    Almost there

--- cachematrix.R
+++ cachematrix.R
@@ -4,40 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-     d <- det(x)
-     if(d == 0)
-     {
-          break()
-     } 
-     inverseTF <- NULL
-     setInverse <- function(y){
-          x <<- solve(y)
-          inverseTF <<- TRUE
+     inverse <- NULL
+     set <- function(y) {
+          x <<- y
+          inverse <<- NULL
      }
      
-     getInverse <- function() {
-          if(isTRUE(inverseTF))
-          {x}
-          else{
-               print('Inverse is not set yet. Please set inverse using makeCacheMatrix function')
-               break()
-          }
-          
-     }
-     setInverse(x)
-     ret <- matrix(c(setInverse = setInverse, getInverse = getInverse), nrow = 1, ncol = 2)
-     ret
+     get <- function() x
+     setinverse <- function(solve) inverse <<- solve
+     getinverse <- function() inverse
+     list(set = set, get = get, 
+          setinverse = setinverse,
+          getinverse = getinverse)
+     
 }
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
      ## Return a matrix that is the inverse of 'x'
-     inverse <- getInverse()
-     if(isTRUE(inverseTF)){
-          print('getting cached matrix')
-          return(inverse, solve(inverse))
-     }
+     m <- x$getinverse()
      
+     if(!is.null(m)) {
+          message("getting cached data")
+          return(m)
+     }
+     data <- x$get()
+     m <- solve(data)
+     x$setinverse(m)
+     m
      
 }
